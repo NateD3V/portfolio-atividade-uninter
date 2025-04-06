85ff5f2d7687cb34bd8230232d737e7363e3730c
Nathan <adm.nate@hotmail.com>
2025-04-06 14:58:56 -0300

    added smooth scroll

--- script.js
+++ script.js
@@ -89,3 +89,13 @@ menuItems.forEach((menuItem) => {
 menuButton.addEventListener("click", () => {
   menu.classList.toggle("active");
 });
+
+// Scroll suave para as seções
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
